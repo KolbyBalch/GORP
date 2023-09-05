69e253b7a7c1fd27d5bb7b5ddeb7344e19648609
KolbyBalch <kolby.balch@gmail.com>
2023-09-04 22:05:26 -0500

    responsive text and nav-bar basics

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/NavBar.module.css'
+
+export default function NavBar() {
+    return (
+        <div className={styles.navBar} id="navBar">
+            This is a nav-bar
+        </div>
+    )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,30 @@
+import NavBar from "@components/NavBar";
+import React, { useEffect } from "react";
+import styles from "../styles/NavBar.module.css"
+
 export default function Home() {
+
+  useEffect(() => {
+    const header = document.getElementById("navBar");
+    const stickyOffset = header.offsetTop;
+
+    window.onscroll = function() {
+      if(window.scrollY > stickyOffset) {
+        header.classList.add(styles.sticky);
+      } else {
+        header.classList.remove(styles.sticky);
+      }
+    }
+  }, [])
+  
   return (
-    <>
+    <main>
       <div className="home-container">
-        <main>
-          <span className="hollow">START</span>
-          <span className="hollow">YOUR</span>
-          <span className="full-title">JOURNEY</span>
-        </main>
+        <span className="hollow-title">START</span>
+        <span className="hollow-title">YOUR</span>
+        <span className="full-title">JOURNEY</span>
       </div>
-    </>
+      <NavBar />
+    </main>
   )
 }
