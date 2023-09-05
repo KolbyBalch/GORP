bf7e72284ceae587ff6caa434d9884718a1fbf2c
KolbyBalch <kolby.balch@gmail.com>
2023-09-04 20:37:09 -0500

    home-page changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,13 @@
+import Head from 'next/head'
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
     return (
         <>
+            <Head>
+                <title>Journey Church MS</title>
+                <link rel="icon" href="/journey-church-logo-circle.png" type="image/png" sizes="any" />
+            </Head>
             <main>{children}</main>
             <Footer />
         </>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,13 @@
-import Head from 'next/head'
-import Footer from 'src/components/Footer'
-
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Journey Church MS</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title" >Welcome to my app!</h1>
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-    </div>
+    <>
+      <div className="home-container">
+        <main>
+          <span className="hollow">START</span>
+          <span className="hollow">YOUR</span>
+          <span className="full-title">JOURNEY</span>
+        </main>
+      </div>
+    </>
   )
 }
