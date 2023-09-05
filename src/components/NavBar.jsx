ac2e854abc90273b4eb207bf8a9f7c240446f89c
KolbyBalch <kolby.balch@gmail.com>
2023-09-04 23:15:31 -0500

    added logo to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,12 @@
 import styles from '../styles/NavBar.module.css'
+import Image from 'next/image'
 
 export default function NavBar() {
     return (
         <div className={styles.navBar} id="navBar">
-            This is a nav-bar
+            <div className={styles.imageContainer}>
+                <Image src='/journey-church-logo.png' alt="Journey Church logo" layout="fill"/>
+            </div>
         </div>
     )
 }
