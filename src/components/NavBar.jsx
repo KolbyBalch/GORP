d699bcb668417c5902b7ccc52741bf0766a91c2c
KolbyBalch <kolby.balch@gmail.com>
2023-11-18 22:19:39 -0600

    added login button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,21 @@
 export default function NavBar() {
     return (
         <div className="nav-bar sticky" id="navBar">
-            <a className="header-link" href='/'>
-                <strong>GORP</strong>
-            </a>
+            <div>
+            </div>
+            <div>
+                <a className="header-link" href='/'>
+                    <strong>GORP</strong>
+                </a>
+            </div>
+            <div className="login-div">
+                <button 
+                    className="login-button" 
+                    onClick={() => console.log("login clicked")}
+                >
+                    Login
+                </button>
+            </div>
         </div>
     )
 }
