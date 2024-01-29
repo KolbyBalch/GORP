1bf37b075e102d2e21f8178fb81e1f9b25be0a6a
KolbyBalch <kolby.balch@gmail.com>
2024-01-29 14:33:57 -0600

    added hidden input

--- src/pages/character-creator/index.jsx
+++ src/pages/character-creator/index.jsx
@@ -5,6 +5,7 @@ export default function CharacterCreator() {
     <div className="home-container">
       <h1>GORP</h1>
       <form name="character" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="character" />
         <label>Character Name: <input type="text" name="name" /></label>
       </form>
     </div>
