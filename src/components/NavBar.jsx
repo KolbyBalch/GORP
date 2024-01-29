f8b2f5551adfd78c41f6c7d79a2886923f9e18f9
KolbyBalch <kolby.balch@gmail.com>
2024-01-29 14:07:21 -0600

    trying another console log

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,6 +7,7 @@ export default function NavBar() {
             setLoggedIn(!!user)
             setUser(user)
             netlifyAuth.closeModal()
+            console.log(netlifyAuth.currentUser)
         })
     }
 
