b8546f40e36ae31075f08295827a28d9cf5086aa
KolbyBalch <kolby.balch@gmail.com>
2024-01-29 22:03:51 -0600

    trying dynamic form name

--- src/pages/character-creator/index.jsx
+++ src/pages/character-creator/index.jsx
@@ -17,8 +17,7 @@ export default function CharacterCreator() {
     <div className="home-container">
       <h1>GORP</h1>
       <form name="character" method="POST" data-netlify="true">
-        <input type="hidden" name="form-name" value="character" />
-        <input type="hidden" name="user-token" value={useStore.getState()?.user?.id} />
+        <input type="hidden" name="form-name" value={useStore.getState()?.user?.id} />
         <label>Character Name: <input type="text" name="name" /></label>
       </form>
     </div>
