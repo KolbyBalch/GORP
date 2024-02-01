73c6fb02c4c045de88aac7df5cc44879774a1622
KolbyBalch <kolby.balch@gmail.com>
2024-02-01 15:05:55 -0600

    changed function

---
+++ functions/create-character.js
@@ -0,0 +1,23 @@
+/* code from functions/todos-create.js */
+import faunadb from 'faunadb' /* Import faunaDB sdk */
+
+/* configure faunaDB Client with our secret */
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: NEXT_CLIENT_SSR_ENTRY_SUFFIX.env.get("FAUNADB_SERVER_SECRET")
+})
+
+export default async (req, context) => {
+  const data = JSON.parse(req.body);
+  const character = {
+    data: data
+  }
+  await client.query(q.Create(q.Ref("classes/character"), character))
+  .then((response) => {
+    console.log("success", response)
+    return new Response(response, { status: 200 })
+  }).catch((error) => {
+    console.log("error", error)
+    return new Response(response, { status: 400 })
+  })
+}
