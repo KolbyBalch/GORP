8ecc5cb652c40b94ea6f5f8308cc6f88e6acad67
KolbyBalch <kolby.balch@gmail.com>
2024-02-03 01:54:21 -0600

    updating function

--- functions/create-character.js
+++ functions/create-character.js
@@ -8,7 +8,7 @@ const client = new faunadb.Client({
 })
 
 export default async (req, context) => {
-  const data = req.body;
+  const data = JSON.parse(req.body.source);
   await client.query(q.Create(q.Ref("classes/character"),
     {
       data: data
